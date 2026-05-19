5ac83f1dd77381578862cc608bda08a616036dad
BrokenSyntax <leshachex@mail.ru>
2026-05-19 21:40:50 +0300

    Remove duplicate camelCase aliases

--- modules/DetectionResult.py
+++ modules/DetectionResult.py
@@ -9,25 +9,14 @@ class DetectionResult:
     bbox: Tuple[int, int, int, int]
     result_id: int = 0
 
-    @property
-    def resultId(self) -> int:
-        return self.result_id
-
-    @property
-    def boundingBox(self) -> Tuple[int, int, int, int]:
-        return self.bbox
-
     def center(self) -> Tuple[int, int]:
         x1, y1, x2, y2 = self.bbox
         return ((x1 + x2) // 2, (y1 + y2) // 2)
 
     def get_result_data(self):
         return {
-            'resultId': self.result_id,
-            'vehicleType': self.vehicle_type,
-            'boundingBox': self.bbox,
+            'result_id': self.result_id,
+            'vehicle_type': self.vehicle_type,
+            'bbox': self.bbox,
             'confidence': self.confidence
         }
-
-    def getResultData(self):
-        return self.get_result_data()
