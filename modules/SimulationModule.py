257eab4241d900085bfb44a2eec2554ef2bab343
BrokenSyntax <leshachex@mail.ru>
2026-05-19 21:44:08 +0300

    Remove duplicate camelCase aliases

--- modules/SimulationModule.py
+++ modules/SimulationModule.py
@@ -3,9 +3,7 @@ from modules.SimulationResult import SimulationResult
 
 class SimulationModule:
     def __init__(self, simulation_id: int = 0, scenario_name: str = 'default'):
-        self.simulationId = simulation_id
         self.simulation_id = simulation_id
-        self.scenarioName = scenario_name
         self.scenario_name = scenario_name
         self.is_running = False
 
@@ -13,15 +11,9 @@ class SimulationModule:
         self.is_running = True
         return True
 
-    def startSimulation(self):
-        return self.start_simulation()
-
     def stop_simulation(self):
         self.is_running = False
         return True
-
-    def stopSimulation(self):
-        return self.stop_simulation()
 
     def compare_modes(self, adaptive_metrics=None, fixed_metrics=None):
         adaptive_metrics = adaptive_metrics or {}
@@ -38,6 +30,3 @@ class SimulationModule:
             'adaptive': SimulationResult(adaptive_delay, adaptive_queue, adaptive_throughput),
             'fixed': SimulationResult(fixed_delay, fixed_queue, fixed_throughput)
         }
-
-    def compareModes(self, adaptive_metrics=None, fixed_metrics=None):
-        return self.compare_modes(adaptive_metrics=adaptive_metrics, fixed_metrics=fixed_metrics)
