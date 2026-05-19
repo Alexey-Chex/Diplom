7d287ebc020319851193dd16e14fa3cc0c955da3
BrokenSyntax <leshachex@mail.ru>
2026-05-19 13:14:00 +0300

    Align FrameProcessor with diploma module diagram

--- modules/frame_processor.py
+++ modules/frame_processor.py
@@ -3,8 +3,14 @@ from typing import Tuple
 
 
 class FrameProcessor:
-    def __init__(self, resize_dim: Tuple[int, int] = (416, 234), frame_skip: int = 2):
+    def __init__(self, resize_dim: Tuple[int, int] = (416, 234), frame_rate: float = 30.0, frame_skip: int = 2):
         self.resize_dim = resize_dim
+        self.frameRate = frame_rate
+        self.frame_rate = frame_rate
+        self.resizeWidth = resize_dim[0]
+        self.resizeHeight = resize_dim[1]
+        self.resize_width = resize_dim[0]
+        self.resize_height = resize_dim[1]
         self.frame_skip = frame_skip
 
     def should_process_frame(self, frame_index: int) -> bool:
@@ -12,6 +18,18 @@ class FrameProcessor:
 
     def resize_frame(self, frame):
         return cv2.resize(frame, self.resize_dim)
+
+    def extract_frames(self, video_data):
+        return video_data.get_frames()
+
+    def extractFrames(self, video_data):
+        return self.extract_frames(video_data)
+
+    def preprocess_frame(self, frame):
+        return self.resize_frame(frame)
+
+    def preprocessFrame(self, frame):
+        return self.preprocess_frame(frame)
 
     def draw_zone(self, frame, zone, color=(255, 0, 0), thickness=2):
         x1, y1, x2, y2 = zone
