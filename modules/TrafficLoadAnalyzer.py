bdeccfc144c7a8d490979774cfbaf4ded21923c2
BrokenSyntax <leshachex@mail.ru>
2026-05-19 21:43:26 +0300

    Remove duplicate camelCase aliases

--- modules/TrafficLoadAnalyzer.py
+++ modules/TrafficLoadAnalyzer.py
@@ -1,8 +1,6 @@
 class TrafficLoadAnalyzer:
     def __init__(self, traffic_density=0.0, queue_length=0.0):
-        self.trafficDensity = traffic_density
         self.traffic_density = traffic_density
-        self.queueLength = queue_length
         self.queue_length = queue_length
 
     def analyze_load(self, direction_counts, wait_times=None):
@@ -17,9 +15,6 @@ class TrafficLoadAnalyzer:
             }
         return metrics
 
-    def analyzeLoad(self, direction_counts, wait_times=None):
-        return self.analyze_load(direction_counts, wait_times)
-
     def calculate_phase_metrics(self, direction_metrics):
         ns_queue = direction_metrics['top']['queue_length'] + direction_metrics['bottom']['queue_length']
         ew_queue = direction_metrics['left']['queue_length'] + direction_metrics['right']['queue_length']
@@ -32,6 +27,3 @@ class TrafficLoadAnalyzer:
 
     def calculate_metrics(self, direction_metrics):
         return self.calculate_phase_metrics(direction_metrics)
-
-    def calculateMetrics(self, direction_metrics):
-        return self.calculate_metrics(direction_metrics)
