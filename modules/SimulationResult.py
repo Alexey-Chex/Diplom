37592d195a419990ddb480d01cbf4db2fddb4444
BrokenSyntax <leshachex@mail.ru>
2026-05-20 22:30:37 +0300

    Remove delay metric from simulation result

--- modules/SimulationResult.py
+++ modules/SimulationResult.py
@@ -3,13 +3,11 @@ from dataclasses import dataclass
 
 @dataclass
 class SimulationResult:
-    average_delay: float = 0.0
     queue_length: float = 0.0
     throughput: float = 0.0
 
     def get_simulation_metrics(self):
         return {
-            'average_delay': self.average_delay,
             'queue_length': self.queue_length,
             'throughput': self.throughput
         }
